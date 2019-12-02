e45c508d718044ef07bd7380ada05167982278cf
dnnagy <nagy@chromeshift.com>
2019-12-02 14:08:26 +0100

    Fix Dockerfile

--- startup.sh
+++ startup.sh
@@ -1,7 +1,11 @@
+set -e
 if [[ -n $THEME ]]; then
-  jt -t $THEME -fs 95 -altp -tfs 11 -nfs 115 -cellw 88% -T -N
+	echo "Setting theme $THEME."
+	# Set theme
+	jt -t $THEME -fs 95 -altp -tfs 11 -nfs 115 -cellw 88% -T -N
 else
-  jt -r
+	echo "No theme specified, resetting jupyter theme."
+  	jt -r
 fi
 cd $WORKDIR
 jupyter notebook --ip=0.0.0.0 --no-browser
