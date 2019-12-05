081d61cf9f077611696b336781af6e236c6587d1
Nagy Dániel <kommancs96@me.com>
2019-12-05 17:01:24 +0100

    Update set_theme.sh

--- set_theme.sh
+++ set_theme.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
-if [[ -n $THEME ]]; then
-    echo "Cahnging to theme $THEME."
+if [[ -n ${THEME} ]]; then
+    echo "Changing to theme ${THEME}."
     jt -t $THEME -fs 95 -altp -tfs 11 -nfs 115 -cellw 88% -T -N
 else
     echo "Resetting theme."
     jt -r
+fi
+if [[ ${PLT_DARK_THEME:-0} -eq 1 ]]; then
+    # Change matplotlib theme to dark theme defined at my github
+    # TODO
+    ;
+else
+    # Restore default matplotlib theme 
+    # TODO
+    ;
 fi
