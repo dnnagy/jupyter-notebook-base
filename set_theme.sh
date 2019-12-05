c93a5278ef27c7eef133081366e56ae09b823f98
Nagy Dániel <kommancs96@me.com>
2019-12-05 17:44:39 +0100

    Update set_theme.sh

--- set_theme.sh
+++ set_theme.sh
@@ -7,11 +7,16 @@ else
     jt -r
 fi
 if [[ ${PLT_DARK_THEME:-0} -eq 1 ]]; then
-    # Change matplotlib theme to dark theme defined at my github
-    # TODO
-    ;
-else
-    # Restore default matplotlib theme 
-    # TODO
-    ;
+    MPL_RC_FILE=$(python -c "import matplotlib; print(matplotlib.matplotlib_fname())")
+    # Create a backup
+    cp MPL_RC_FILE /home/${NB_USER}/matplotlibrc.backup
+    # Write the content of my matplotlibrc file into the original one
+    if [[ -n ${MPL_RC_FILE_DARK} ]]; then
+        echo "ERROR: Environment variable MPL_RC_FILE_DARK must exist"; exit -1
+    fi
+    # Check if file size > 0
+    if [[ -s ${MPL_RC_FILE_DARK} ]]; then
+        echo "ERROR: File ${MPL_RC_FILE_DARK} must not be empty"; exit -1
+    fi
+    cat ${MPL_RC_FILE_DARK} > MPL_RC_FILE
 fi
