f23393f6e39fe21b1d4e68b3b46a1adc55d55759
Nagy Dániel <kommancs96@me.com>
2020-03-08 13:01:38 +0100

    lmao sleep 0.1s

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,5 +3,6 @@ su $NB_USER bash -c '/home/${NB_USER}/set_theme.sh || echo -e "${RED}ERROR: set_
 # WARNING: The init_notebook.sh must be run after set_theme.sh!
 su $NB_USER bash -c '/home/${NB_USER}/init_notebook.sh || echo -e "${RED}ERROR: init_notebook.sh failed.${NC}"'
 su $NB_USER bash -c 'cd $WORKDIR && nohup sh -c "jupyter notebook --ip=0.0.0.0 --no-browser" > /home/$NB_USER/jupyter.log &'
-su $NB_USER bash -c 'sleep 1s'
+# I don't know why I added this line, but ok, let's sleep.
+su $NB_USER bash -c 'sleep 0.1s'
 /bin/bash
